22417ae82c70a6628ad77f4cfba479519cab82fa
santi <santiago.verdu.01@gmail.com>
2016-03-21 21:27:08 -0600

    hard coded a thing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var dao = require("../models/DAO");
-  res.render('index', { contacts: [] });
+  var hardCoded = [
+    {
+      name: "santi",
+      phone: "9737273220",
+      email: "santiago.verdu.01@gmail.com",
+      photo: "google.com",
+      address: "186 Lake Shore Drive"
+    }
+  ]
+  res.render('index', { contacts: hardCoded });
 });
 
 module.exports = router;
