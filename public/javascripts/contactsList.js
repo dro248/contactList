5d70885c061e29f706f7f93aa8de785772a0c0a5
santi <santiago.verdu.01@gmail.com>
2016-03-22 01:16:57 -0600

    got the contacts to load in phone

--- public/javascripts/contactsList.js
+++ public/javascripts/contactsList.js
@@ -1,18 +1,18 @@
-(function() {
-  var app = angular.module('contactsListApp', []);
+var app = angular.module('contactsListApp', []);
 
-  app.controller('ContactsListController', function($scope){
-      $scope.contactsList = [];
-      var responseBody;
-      var xhttp = new XMLHttpRequest();
+app.controller('ContactsListController', function($scope){
+    $scope.contacts = [];
 
-      xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-          responseBody = xhttp.responseText;
-        }
-      };
-
-      xhttp.open("GET", "/contacts/list", false);
-      xhttp.send();
-  });
-})();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        var list = JSON.parse(xhttp.responseText);
+        list.forEach(function(contact) {
+          $scope.contacts.push(contact);
+        });
+        $scope.$apply();
+      }
+    };
+    xhttp.open("GET", "/contacts/list");
+    xhttp.send();
+});
