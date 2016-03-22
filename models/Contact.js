1480f6ae505d218542ecff1f043fec5b2500bb25
santi <santiago.verdu.01@gmail.com>
2016-03-21 22:18:39 -0600

    Working on adding and removing contacts backend

--- models/Contact.js
+++ models/Contact.js
@@ -11,33 +11,48 @@ var contactSchema = dao.Schema({
 
 var Contact = dao.model('Contact', contactSchema);
 
-// Contact database access methods go here
 module.exports = {
   /**
    * Get all of the contacts from the contacts collection
    */
   getAll: function() {
-
     Contact.find(function(err, contactList) {
-      console.log(contactList);
-    });
-
-    var hardCoded = [
-      {
-        name: "santi",
-        phone: "9737273220",
-        email: "santiago.verdu.01@gmail.com",
-        photo: "google.com",
-        address: "186 Lake Shore Drive"
+      if (err) {
+        console.log(err);
       }
-    ];
-    return hardCoded;
+      return contactList || [];
+    });
   },
   /**
    * Inserts a contact into the contacts collection
    * a contact has a name, phone, email, photo, address
    */
   addContact: function(contact) {
-    Contact.insert(contact);
+    var contact = new Contact(contact);
+    contact.save(function(err) {
+      if (err) {
+        console.log(err);
+        return false;
+      } else {
+        console.log("Contact Saved.");
+        return true;
+      }
+    });
+  },
+  /**
+   *  Delete a contact from the database based on the _id field
+   */
+  removeContact: function(contactId) {
+    contact.findOne({_id: contactId}, function(err, contact) {
+      console.log("delete contact");
+      console.log(contact);
+      contact.remove(function(err) {
+        if (err) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+    });
   }
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-var Contact = require("../models/Contact");
+var contacts = require("../models/Contact");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { contacts: Contact.getAll() });
+  res.render('index');
 });
 
+/* GET contacts */
+router.get('/contacts/list', function(req, res, next) {
+  res.json(contacts.getAll());
+});
+
+/* POST add a contact to the database */
 router.post('/addContact', function(req, res, next) {
   console.log("This is the request Body: " + req.body);
+  if (contacts.addContact(req.body.contact)) {
+    res.status(200).send("success");
+  } else {
+    res.status(500).send("error");
+  }
+});
+
+router.post('/delete/contact', function(req, res, next) {
+  console.log("This is the request Body for the: " + req.body);
+  if (contacts.removeContact(req.body.contactId)) {
+    res.status(200).send("success");
+  } else {
+    res.status(500).send("error");
+  }
 });
 
 module.exports = router;
