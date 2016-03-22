e219b151642fa897aebe3829bf248459649682e4
santi <santiago.verdu.01@gmail.com>
2016-03-21 18:43:24 -0600

    Moved stles and index.html

---
+++ models/DAO.js
@@ -0,0 +1,13 @@
+/*********************************************************\
+  Set up mongoose in order to connect to mongo database 
+\*********************************************************/
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/recipes');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() { console.log('Connected to database.'); });
+
+module.exports = mongoose;
