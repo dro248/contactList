e10227c5677fd80cd81d63cc1d5c01dc52f8b446
santi <santiago.verdu.01@gmail.com>
2016-03-22 01:27:48 -0600

    data integrity

--- routes/index.js
+++ routes/index.js
@@ -12,19 +12,31 @@ router.get('/', function(req, res, next) {
 router.get('/contacts/list', function(req, res, next) {
   var listPromise = contacts.getAll();
   listPromise.then(function(list) {
+    var filteredList = list.filter(is_valid);
     res.setHeader('Content-Type', 'application/json');
-    res.json(list);
+    res.json(filteredList);
   });
 });
 
 /* POST add a contact to the database */
 router.post('/addContact', function(req, res, next) {
-  if (contacts.addContact(req.body)) {
-    res.status(200).send("success");
+  if (is_valid(req.body)) {
+    if (contacts.addContact(req.body)) {
+      res.status(200).send("success");
+    } else {
+      res.status(500).send("error");
+    }
   } else {
-    res.status(500).send("error");
+    res.status(400).send("error");
   }
 });
+
+function is_valid(c) {
+  if (c.name === "" || (c.phone === "" && c.address === "")) {
+    return false;
+  }
+  return true;
+}
 
 router.post('/delete/contact', function(req, res, next) {
   console.log("This is the request Body for the: " + req.body);
